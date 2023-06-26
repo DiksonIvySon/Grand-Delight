e646a253ca12431b2f0a46b381681b04f449ae39
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-06-26 17:35:37 +0300

    Simpplify the boilerplate app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-
+import React, {Component} from 'react';
 import './App.css';
 
+class APP extends Component {
+  constructor() {
+    super();
+  }
 
+  render() {
+    return(
+      <>
+      <h1>Hello React</h1>
+      </>
+    );
+  }
+}
 
 export default App;
