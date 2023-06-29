bb5d4bbd35e2ee83756927f87d4d5bf060ba4b18
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-06-29 14:03:05 +0300

    Created an object to store the information of each food type in the week specials section

--- src/Components/main.js
+++ src/Components/main.js
@@ -4,32 +4,30 @@ import bruchetta from './assets/bruchetta.svg'
 import lemonDessert from './assets/lemonDessert.jpg'
 
 
-const foodTypeArray = [
-        {id: greekSalad,
-        title: "Greek Salad",
-        image: greekSalad,
-        price: 12.99,
-        description: "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons."
-    },
-        {id: bruchetta,
-        title: "Bruchetta",
-        image: bruchetta ,
-        price: 5.99,
-        description: "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. " ,
-    },
-        {id: lemonDessert,
-        title: "Lemon Dessert",
-        image: lemonDessert,
-        price: 5.00,
-        description: "This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined." ,
-    }
-];
+/*const foodTypeArray = {
+    greekSalad: {title: "Greek Sala",
+                image: greekSalad,
+                price: 12.99,
+                description: "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons."
+                },
+    bruchetta: {title: "Bruchetta",
+                image: bruchetta ,
+                price: 5.99,
+                description: "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. " ,
+               },
+    lemonDessert: {title: "Lemon Dessert",
+                   image: lemonDessert,
+                   price: 5.00,
+                   description: "This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined." ,
+                  }
+};*/
 
-function FoodCard() {
+function FoodCard(props) {
+    const foodType = props.foodType;
 
     return(
         <div className="card">
-            <img src={greekSalad}></img>
+            <img src={foodType}></img>
             <div className="card-tittle">
                 <div>
                     <h4>Greek Salad</h4>
@@ -53,10 +51,10 @@ function FoodCard() {
 
 class Main extends Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
     }
-
+ 
     render() {
         return(
             <main className="main">
@@ -70,9 +68,9 @@ class Main extends Component {
                         </div>
                     </div>
                     <div className="food-card">
-                        <FoodCard />
-                        <FoodCard />
-                        <FoodCard />
+                        <FoodCard foodType={bruchetta}/>
+                        <FoodCard foodType={greekSalad}/>
+                        <FoodCard foodType={lemonDessert}/>
                     </div>
                 </div>
             </main>
